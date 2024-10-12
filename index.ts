249c6871881e00ada4b63fbb7a04e2a871f49e80
ifanhaqq <haqinhanifan@gmail.com>
2024-10-12 17:03:49 +0700

    neraca air index

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import brokerConnectivity from "./src/services/brokerConnectivity";
 import { QueryHandler } from "./src/services/QueryHandler";
 import bodyParser from "body-parser";
+import countNeracaAir from "./src/queries/countNeracaAir";
 
 dotenv.config();
 
@@ -13,7 +14,8 @@ brokerConnectivity(publishing);
 
 async function main() {
     const app: Express = express();
-    const port = process.env.PORT || 3000;
+    const port = 3000;
+    const host = '0.0.0.0';
     const queryHandler: QueryHandler = new QueryHandler();
 
     app.use(express.json());
@@ -33,6 +35,13 @@ async function main() {
         res.send(todayRain);
     });
 
+    app.post('/neraca', async (req: Request, res: Response) => {
+        const data:any = await countNeracaAir(parseInt(req.body.month));
+
+        console.log(data)
+        res.json(data)
+    });
+
     app.post('/specified-time', async (req: Request, res: Response) => {
         const date:any = req.body.date;
         (!date) ? res.send("Please input some date") : res.send(req.body);
@@ -46,8 +55,21 @@ async function main() {
         res.send(history);
     });
 
-    app.listen(port, () => {
-        console.log(`[server]: Server is running at http://localhost:${port}`);
+    app.post('/rain-history-calendar', async (req: Request, res: Response) => {
+        const date = new Date(req.body.end_day);
+        const day = req.body.day;
+
+        try {
+            const history: any = await queryHandler.rainHistory(date, day);
+            res.send(history);
+        } catch (error) {
+            res.send(error)
+        }
+        
+    })
+
+    app.listen(port, host, () => {
+        console.log(`[server]: Server is running at http://${host}:${port}`);
     });
 }
 
